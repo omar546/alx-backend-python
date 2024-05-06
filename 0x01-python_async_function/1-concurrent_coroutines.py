232650145ad5660e6376e885d49c91dfe3808753
omar546 <omer.mkawy.om@gmail.com>
2024-05-06 20:36:48 +0300

    s

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Let's execute multiple coroutines at the same time with async
+Let's execute multiple coroutines
+at the same time with async
 """
 import asyncio
 import random
